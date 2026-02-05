8e7aa6ae8140b7fa2fe257450ea161f63ffd8f97
snezhka003 <you@example.com>
2026-02-05 21:20:11 +0300

    added solving the task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
-  let arr = [];
-  
+  let arr;
+  arr = [];
+
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d === 0) {
+    arr.push(-b/(2*a));
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d) )/(2*a));
+    arr.push((-b - Math.sqrt(d) )/(2*a));
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let totalAmount;
+
+  let credit = amount - contribution;
+  let rate = percent/12/100;
+  let monthPay = credit * (rate + (rate / (((1 + rate) ** countMonths) - 1)));
+
+  totalAmount = (monthPay * countMonths).toFixed(2);
+
+  return +totalAmount;
 }

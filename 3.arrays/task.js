f56a23ec718c0f3ad23a8244c1caef3a8e0584d6
snezhka003 <you@example.com>
2026-02-07 19:24:54 +0300

    added solving the tasks

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]); // boolean
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter(object => object.gender == gender).map(object => object.age);
+
+  let res;
+
+  if (result.length < 1) {
+    return res = 0;
+  } else {
+    res = result.reduce((a, b) => a + b) / result.length;
+  }  
+
+  return res;
 }

958c55aec7ba0434e65c9dba465ee473281d59e1
snezhka003 <you@example.com>
2026-02-08 16:29:29 +0300

    updated solving the second task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -3,7 +3,28 @@ function compareArrays(arr1, arr2) {
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  let result = users.filter(object => object.gender == gender).map(object => object.age);
+  //  при использовании reduce напрямую в цепочке не получается реализовать правильную проверку на пустой массив и избежать деления на 0, как результат - ошибка тестов с пустыми массивами
+// просьба подсказать что я делаю не так в данном подходе.
+
+  /*   let result = users.filter(object => object.gender === gender).map(object => object.age).reduce((acc, item, index, arr) => {
+    if (arr.length < 1) {
+      return result = 0;
+    } else {
+      acc += item;
+
+      if (index === arr.length - 1) {
+        return acc / arr.length;      
+      }
+
+      return acc;
+    }
+  });
+
+  return result; */
+
+  // при вынесении вычисления среднего арифметического в отдельную переменную получается реализовать проверку на пустой массив,и все тесты, включая пустые массивы, проходят успешно
+  
+  let result = users.filter(object => object.gender === gender).map(object => object.age);
 
   let res;
 

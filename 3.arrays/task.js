b4c37e86aedfc7586bf3e6dab98bd6b26504a5e4
snezhka003 <you@example.com>
2026-02-11 09:45:46 +0300

    new update solving the second task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,38 +1,21 @@
 function compareArrays(arr1, arr2) {
-  return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]); // boolean
+	return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]); // boolean
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  //  при использовании reduce напрямую в цепочке не получается реализовать правильную проверку на пустой массив и избежать деления на 0, как результат - ошибка тестов с пустыми массивами
-// просьба подсказать что я делаю не так в данном подходе.
+// длинный вариант с использованием условия с индексом, как в лекции
+    return users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, age, index, arr) => {
+        acc += age;
 
-  /*   let result = users.filter(object => object.gender === gender).map(object => object.age).reduce((acc, item, index, arr) => {
-    if (arr.length < 1) {
-      return result = 0;
-    } else {
-      acc += item;
+        if (index === arr.length - 1) {
+            return acc / arr.length;      
+        }
 
-      if (index === arr.length - 1) {
-        return acc / arr.length;      
-      }
+        return acc;
+    }, 0);
 
-      return acc;
-    }
-  });
-
-  return result; */
-
-  // при вынесении вычисления среднего арифметического в отдельную переменную получается реализовать проверку на пустой массив,и все тесты, включая пустые массивы, проходят успешно
-  
-  let result = users.filter(object => object.gender === gender).map(object => object.age);
-
-  let res;
-
-  if (result.length < 1) {
-    return res = 0;
-  } else {
-    res = result.reduce((a, b) => a + b) / result.length;
-  }  
-
-  return res;
+// короткий вариант без использования условия с индексом
+/*     return users.filter(user => user.gender === gender)
+        .map(user => user.age)
+        .reduce((acc, age, arr) => acc + age / arr.length, 0); */
 }
